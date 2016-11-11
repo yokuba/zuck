168cc9ca044df487e963da7ca1096725134281f2
Peter Clark <peter@5clarks.net>
2016-11-10 19:22:53 -0600

    objectives

--- lib/zuck/facebook/campaign.rb
+++ lib/zuck/facebook/campaign.rb
@@ -1,6 +1,10 @@
 module Zuck
   class Campaign < RawFbObject
 
+    OBJECTIVES = %w(
+      BRAND_AWARENESS CANVAS_APP_ENGAGEMENT CANVAS_APP_INSTALLS CONVERSIONS EVENT_RESPONSES EXTERNAL LEAD_GENERATION LINK_CLICKS LOCAL_AWARENESS, MOBILE_APP_ENGAGEMENT MOBILE_APP_INSTALLS OFFER_CLAIMS PAGE_LIKES POST_ENGAGEMENT PRODUCT_CATALOG_SALES REACH VIDEO_VIEWS
+    )
+
     # https://developers.facebook.com/docs/marketing-api/reference/ad-campaign-group
     known_keys :id,
                :account_id,
